556ecbd8cc8a57c36d8671bf7568b7872cdcdce9
Phan Loc <ginga550504@gmail.com>
2021-10-12 21:17:18 +0700

    update

--- src/server.js
+++ src/server.js
@@ -21,5 +21,5 @@ app.use(express.static('./src/public'));
 
 let port = process.env.PORT || 3999;
 app.listen(port, () => {
-  console.log(`Server started on port http://localhost:${port}//`);
+  console.log(`Server started on port http://localhost:${port}/`);
 })
